a87d00a28b8539f4e2b6ee256083ada052dafd60
duc huy <truongduchuy.1903@gmail.com>
2025-06-04 20:46:34 +0700

    edit main.py

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ from app.etl.transform.transform import transform_heart_disease_data
 if __name__ == "__main__":
     extract_heart_disease_data()
     extract_data_to_csv()
-    stats = transform_heart_disease_data()
     
